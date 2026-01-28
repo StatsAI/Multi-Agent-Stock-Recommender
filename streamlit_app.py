7b4caf36a96e44084886f3a19df30c19014962c6
StatsAI <67183539+StatsAI@users.noreply.github.com>
2026-01-28 03:23:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ st.write('')
 # --- 1. SETTINGS & UI ---
 st.set_page_config(page_title="AI Wall Street Team", layout="wide")
 
-st.title("🤖 Multi-Agent AI Wall Street Team 🚀🚀🚀")
+st.title("🤖 Multi-Agent AI Wall Street Team 🚀🌕")
 st.markdown("""
 This app use LangGraph to create a team of specialized AI agents running in parallel reporting to a supervisor agent.  
 
