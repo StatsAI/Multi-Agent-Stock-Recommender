7676da4941d164567e2f7bfa11e78b3f22d430bb
StatsAI <67183539+StatsAI@users.noreply.github.com>
2026-01-28 03:17:42 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ st.markdown(
             display: block;
             margin-left: auto;
             margin-right: auto;
-	    margin-top: -50px;
+	    margin-top: -25px;
             width: 100%;
 	    #margin: 0;	         		
         }
